009f73233c1e73d5ee5b1e097eef9feed4bdf84f
subrataroy912 <subrata.lastChance@gmail.com>
2026-09-10 22:01:19 -0700

    update public routes

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -2,13 +2,13 @@ import { Navigate, Outlet } from "react-router";
 import { useAuth } from "../context/AuthContext";
 
 export default function PublicRoute() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
 
-  // If they are logged in, send them straight to the dashboard
-  // Otherwise, let them see the public page (Outlet)
-  return isAuthenticated ? (
-    <Navigate to="/dashboard" replace />
-  ) : (
-    <Outlet />
-  );
+  if (isLoading) {
+    return <div className="flex items-center justify-center">Loading...</div>;
+  }
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" replace />;
+  }
+  return <Outlet />;
 }
